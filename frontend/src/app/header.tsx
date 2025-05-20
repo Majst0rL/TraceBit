20a597cb0b27824d12c6c70c0a099f53fd64dde7
MajstorL <majstorovicluka20@gmail.com>
2025-05-20 20:45:51 +0200

    popravek headera

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-6 py-4 shadow-sm" style={{ backgroundColor: 'rgb(56, 56, 56)' }}>
-      <div className="font-bold text-lg text-white"><Link href="/">BrowserFingerprint</Link></div>
+      <div className="font-bold text-lg text-white"><Link href="/">TraceBit</Link></div>
       <div className="space-x-6">
         <Link href="/podatki" className="text-blue-600">Podatki</Link>
       </div>
